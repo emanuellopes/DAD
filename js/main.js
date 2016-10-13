31af5f59379d8752820a8de2e36bcacc4cb27c60
Emanuel Lopes <emanuel.lopes.pt@gmail.com>
2016-10-13 16:28:02 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// 2140825- Emanuel Lopes
+// 2140816- Ruben Domingues
+// 2141387- Ruben Pereira
+
+// Implementation:
+'use strict';
+(function(){
+
+	$('#authors-section .photo-zone:last-child').hide();
+	// User 1
+	$('.photo-zone:nth-child(2) h3','#authors-section ').text("2140825");
+	$('.photo-zone:nth-child(2) p','#authors-section ').text("Emanuel Lopes");
+	// User 2
+	$('.photo-zone:nth-child(3) h3','#authors-section ').text("2140816");
+	$('.photo-zone:nth-child(3) p','#authors-section ').text("Ruben Domingues");
+	// User 3
+	$('.photo-zone:nth-child(4) h3','#authors-section ').text("2141387");
+	$('.photo-zone:nth-child(4) p','#authors-section ').text("Ruben Pereira");
+})();
+
+
